c8f3f8ce5d9a99c394e548f73955bdddcb8d8633
CecBazinga <61522609+CecBazinga@users.noreply.github.com>
2020-03-11 17:55:52 +0100

    Hello world program version 1.0.0

---
+++ src/logic/HelloWorld.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class HelloWorld {
+	
+	public void hello() {
+		
+		//Hello world print
+		
+		System.out.println("This is the real hello world!");
+		
+	}
+
+}
---
+++ src/logic/Main.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HelloWorld saluta = new HelloWorld();
+		
+		saluta.hello();
+		
+	}
+
+}
